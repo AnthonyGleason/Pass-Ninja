793bdf0b6325deef7ddb030bfb72830d0e95f597
Anthony Infortunio <contact@anthonyinfortun.io>
2023-06-26 16:05:03 -0400

    clean up express generator

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+import {Request,Response,NextFunction} from 'express';
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req: Request, res:Response, next:NextFunction) {
   res.render('index', { title: 'Express' });
 });
 
