01e5a946c7a96bbabcee1b9bedcc56fb6fb14544
Anthony Infortunio <contact@anthonyinfortun.io>
2023-09-05 12:00:26 -0400

    update home, login, register css

--- client/src/Components/Home/Home.tsx
+++ client/src/Components/Home/Home.tsx
@@ -45,7 +45,7 @@ export default function Home({vaultController}:{vaultController:VaultController}
           <p>
             <span className='info-block-one' data-aos='fade-right'>Ready to enhance your digital security?</span>
             <br />
-            <span className='info-block-two' data-aos='fade-left'>Tired of complicated password managers?</span>
+            <span className='info-block-two' data-aos='fade-right'>Tired of complicated password managers?</span>
             <br />
             <span className='info-block-three' data-aos='fade-right'>Pass Ninja features the latest two-factor authentication technology and cutting edge password generation technology designed to keep you in complete control every step of the way.</span>
           </p>
