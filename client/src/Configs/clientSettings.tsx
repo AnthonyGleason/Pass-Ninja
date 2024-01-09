7f2d167e2d3d7efa01ca2a45749acb4cd75f6c23
Anthony <anthony@LAPTOP-JPRH7SUD.>
2024-01-08 21:39:48 -0500

    update api endpoint url

--- client/src/Configs/clientSettings.tsx
+++ client/src/Configs/clientSettings.tsx
@@ -2,5 +2,5 @@ const USE_LOCALHOST:boolean = true;
 
 export const getFetchURL = function():string{
   if (USE_LOCALHOST) return 'http://localhost:5000';
-  return 'https://pass-ninja-519d509ea28a.herokuapp.com'
+  return 'https://pass-ninja-4d4da81ae607.herokuapp.com'
 }
